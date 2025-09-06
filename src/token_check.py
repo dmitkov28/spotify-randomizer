69a13674ed314956beaf0c003a6853c7a685692f
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-09-06 18:19:18 +0300

    feat: update spotify token manager to handle user access tokens

--- src/token_check.py
+++ src/token_check.py
@@ -1,5 +1,5 @@
-import os
-from dotenv import load_dotenv, set_key
+from base64 import b64encode
+from dotenv import get_key, set_key
 import httpx
 
 
@@ -7,46 +7,48 @@ class SpotifyTokenManager:
     check_endpoint = "https://api.spotify.com/v1/search?q=test&type=artist&limit=1"
     token_endpoint = "https://accounts.spotify.com/api/token"
 
-    env_key = "SPOTIFY_API_TOKEN"
-    client_secret_key = "SPOTIFY_CLIENT_SECRET"
-    client_id_key = "SPOTIFY_CLIENT_ID"
+    app_token_env_key = "SPOTIFY_API_TOKEN"
+    client_secret_env_key = "SPOTIFY_CLIENT_SECRET"
+    client_id_env_key = "SPOTIFY_CLIENT_ID"
+
+    user_id_env_key = "SPOTIFY_USER_ID"
+    user_access_token_env_key = "SPOTIFY_USER_ACCESS_TOKEN"
+    user_refresh_token_env_key = "SPOTIFY_USER_REFRESH_TOKEN"
 
     def __init__(self) -> None:
-        self.token = self._get_token_from_env()
-        self.client_id = self._get_client_id_from_env()
-        self.client_secret = self._get_secret_from_env()
+        self.app_token = self._get_token_from_env(self.app_token_env_key)
+        self.user_access_token = self._get_token_from_env(
+            self.user_access_token_env_key
+        )
+        self.user_refresh_token = self._get_token_from_env(
+            self.user_refresh_token_env_key
+        )
 
-    def _get_token_from_env(self):
-        load_dotenv()
-        token = os.getenv(self.env_key)
-        if not token:
-            raise ValueError("No API TOKEN set")
+        self.client_id = self._get_token_from_env(self.client_id_env_key)
+        self.client_secret = self._get_token_from_env(self.client_secret_env_key)
+
+        self.user_id = self._get_token_from_env(self.user_id_env_key)
+
+    def _get_token_from_env(self, token_name):
+        token = get_key(".env", token_name)
         return token
 
-    def _get_secret_from_env(self):
-        load_dotenv()
-        secret = os.getenv(self.client_secret_key)
-        if not secret:
-            raise ValueError("No Client Secrent set")
-        return secret
+    def set_token_to_env(self, token_name, token):
+        set_key(".env", token_name, token)
 
-    def _get_client_id_from_env(self):
-        load_dotenv()
-        client_id = os.getenv(self.client_id_key)
-        if not client_id:
-            raise ValueError("No Client Id set")
-        return client_id
-
-    def set_token_to_env(self, token):
-        set_key(".env", "SPOTIFY_API_TOKEN", token)
-
-    def _is_token_valid(self):
+    def _is_app_token_valid(self):
         res = httpx.get(
-            self.check_endpoint, headers={"Authorization": f"Bearer {self.token}"}
+            self.check_endpoint, headers={"Authorization": f"Bearer {self.app_token}"}
         )
         return res.status_code == 200
 
-    def _get_new_token(self):
+    def _is_user_access_token_valid(self):
+        url = "https://api.spotify.com/v1/me"
+        headers = {"Authorization": f"Bearer {self.user_access_token}"}
+        response = httpx.get(url, headers=headers)
+        return response.status_code == 200
+
+    def _get_new_app_token(self):
         new_token_res = httpx.post(
             self.token_endpoint,
             headers={
@@ -64,8 +66,26 @@ class SpotifyTokenManager:
 
         return new_token_res.json().get("access_token")
 
-    def validate_token(self):
-        if self._is_token_valid():
+    def _get_new_user_access_token(self):
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Authorization": "Basic "
+            + b64encode(f"{self.client_id}:{self.client_secret}".encode()).decode(),
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+        data = {"grant_type": "refresh_token", "refresh_token": self.user_refresh_token}
+        response = httpx.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        return response.json().get("access_token")
+
+    def validate_app_token(self):
+        if self._is_app_token_valid():
             return
-        new_token = self._get_new_token()
-        self.set_token_to_env(new_token)
+        new_token = self._get_new_app_token()
+        self.set_token_to_env(self.app_token_env_key, new_token)
+
+    def validate_user_token(self):
+        if self._is_user_access_token_valid():
+            return
+        new_token = self._get_new_user_access_token()
+        self.set_token_to_env(self.user_access_token, new_token)
