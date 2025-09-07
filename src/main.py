ee1974cfbe239f42798be7afeda2cd2b01be1fee
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-09-07 14:30:31 +0300

    feat: update main

--- src/main.py
+++ src/main.py
@@ -1,28 +1,10 @@
 from dotenv import load_dotenv
 from spotify import (
-    add_to_playlist,
-    create_spotify_playlist,
-    get_random_tracks,
-    get_track_ids,
-    process_fetched_tracks,
+    SpotifyClient,
 )
-from token_check import SpotifyTokenManager
+
 
 if __name__ == "__main__":
     load_dotenv()
-    
-    playlist_name = "Test"
-
-    sm = SpotifyTokenManager()
-    sm.validate_app_token()
-    sm.validate_user_token()
-    
-    playlist = create_spotify_playlist(
-        sm.user_id, playlist_name=playlist_name, access_token=sm.user_access_token
-    )
-    playlist_id = playlist.get("id")
-    tracks = get_random_tracks(str(sm.app_token))
-    processed_tracks = process_fetched_tracks(tracks)
-    track_ids = get_track_ids(processed_tracks)
-
-    add_to_playlist(playlist_id, track_ids, access_token=str(sm.user_access_token))
+    client = SpotifyClient()
+    client.create_playlist_with_random_tracks(playlist_name="Test Playlist #3")
