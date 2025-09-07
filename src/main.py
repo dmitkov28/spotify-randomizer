b06d9b006cc1d939cf68b12da17bff3706f4ccf2
dmitkov28 <mitkov.dimitar@yahoo.com>
2025-09-07 15:13:17 +0300

    feat: use auto-generated date str in the playlist's name

--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ from dotenv import load_dotenv
 from spotify import (
     SpotifyClient,
 )
+from utils import generate_datetime_str
 
 
 if __name__ == "__main__":
     load_dotenv()
     client = SpotifyClient()
-    client.create_playlist_with_random_tracks(playlist_name="Test Playlist #3")
+    playlist_name = f"Random Playlist {generate_datetime_str()}"
+    client.create_playlist_with_random_tracks(playlist_name=playlist_name)
+    print("✅ Successfully created playlist.")
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,5 @@
 from datetime import datetime
 
+
 def generate_datetime_str(date=datetime.today()) -> str:
     return date.strftime("%d-%m-%Y")
-
-
-print(generate_datetime_str())
-    
